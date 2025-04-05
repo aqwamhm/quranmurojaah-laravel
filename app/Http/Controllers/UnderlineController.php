0a6c78e5ed38ea0faae5acef7ab405e545a3100a
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2025-04-05 12:43:05 +0700

    Add verse relation for underline

--- app/Http/Controllers/UnderlineController.php
+++ app/Http/Controllers/UnderlineController.php
@@ -9,7 +9,7 @@ class UnderlineController extends Controller
 {
     public function show($id)
     {
-        $result = Underline::where('group_id', $id)->get();
+        $result = Underline::where('group_id', $id)->with('verse')->get();
 
         return response()->json(["success" => true, "data" => $result]);
     }
--- app/Models/Underline.php
+++ app/Models/Underline.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Underline extends Model
 {
-    //
+    public function verse()
+    {
+        return $this->belongsTo(Verse::class);
+    }
 }
