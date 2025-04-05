2e639daf6bf2aa7356b879d008673953852f5bb1
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2025-04-05 23:20:59 +0700

    Add string key type for HighlightGroup

--- app/Models/HighlightGroup.php
+++ app/Models/HighlightGroup.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class HighlightGroup extends Model
 {
     public $incrementing = false;
+    protected $keyType = 'string';
 
     public function highlights()
     {
